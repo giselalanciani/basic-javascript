536319b8936420f3c29e80b17034dc182d2f7e25
Gisela Lanciani <giselalanciani@gmail.com>
2021-11-10 12:04:53 -0300

    0027 - replacing-if-else-chains-with-switch.js

---
+++ replacing-if-else-chains-with-switch.js
@@ -0,0 +1,31 @@
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  
+    switch (val) {
+      case "bob":
+      answer = "Marley";
+      break;
+    } switch (val) {
+      case 42:
+      answer = "The Answer";
+      break;
+    } switch(val ) {
+      case 1:
+      answer = "There is no #1";
+      break;
+    } switch (val) {
+      case 99:
+      answer = "Missed me by this much!";
+      break;
+    } switch(val) {
+      case 7:
+      answer = "Ate Nine";
+      break;
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
