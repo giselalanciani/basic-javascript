5b978c3a722d1084b9885b8b1540dba94e14203d
Gisela Lanciani <giselalanciani@gmail.com>
2021-11-11 12:45:10 -0300

    0040 - manipulating-complex-objects.js

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+  },
+  
+   {
+     "artist": "Boom",
+     "title": "See you",
+     "release_year": 2003,
+     "formats": [
+       "Spotify",
+       "Youtube",
+       "CD"
+     ]
+   } 
+  ]
