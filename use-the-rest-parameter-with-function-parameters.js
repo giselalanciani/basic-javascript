a3ae11b32ed86f6d2e1dbcc533aa2469b8e1b37e
Gisela Lanciani <giselalanciani@gmail.com>
2021-11-16 14:16:34 -0300

    0088 - use-the-rest-parameter-with-function-parameters.js

--- use-the-rest-parameter-with-function-parameters.js
+++ use-the-rest-parameter-with-function-parameters.js
@@ -1,6 +1,20 @@
-const sum = (...args) => {
-    return args.reduce((a, b) => a + b, 0);
+//const sum = (...args) => {
+//    return args.reduce((a, b) => a + b, 0);
+//}
+
+class Vegatable {
+    constructor(name){
+    this.name = name;
+    } 
+      
   }
+  // Only change code above this line
   
+  const carrot = new Vegetable('carrot');
+  console.log(carrot.name); // Should display 'carrot'
+
+
+
+
 
 
