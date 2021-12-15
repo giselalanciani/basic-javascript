3107e28d88a5df734fbfa72498ba7f73fbf09e1d
Gisela Lanciani <giselalanciani@gmail.com>
2021-12-15 16:14:46 -0300

    00218 - return-largest-numbers-in-arrays.js

---
+++ return-largest-numbers-in-arrays.js
@@ -0,0 +1,6 @@
+function largestOfFour(arr) {
+    return arr.map(x => x.sort((a, b) => b - a)[0]);
+   };
+   
+   
+   largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
