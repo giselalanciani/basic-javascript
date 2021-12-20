6b44757ebb3b986827d495acba0f2c90baa9cd8e
Gisela Lanciani <giselalanciani@gmail.com>
2021-12-20 10:00:21 -0300

    00239 - sorted-union.js

---
+++ sorted-union.js
@@ -0,0 +1,22 @@
+function uniteUnique(arr) {
+    var finalArray = [];
+
+  
+  for (var i = 0; i < arguments.length; i++) {
+    var arrayArguments = arguments[i];
+
+    
+    for (var j = 0; j < arrayArguments.length; j++) {
+      var indexValue = arrayArguments[j];
+
+      
+      if (finalArray.indexOf(indexValue) < 0) {
+        finalArray.push(indexValue);
+      }
+    }
+  }
+
+  return finalArray;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
